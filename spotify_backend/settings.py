6dd16426d29c6bad6d991bb443367976b4f8b360
Vishnu0074 <vishnusajan71@gmail.com>
2025-07-05 15:44:44 +0530

    Updated settings.py with new configurations

--- spotify_backend/settings.py
+++ spotify_backend/settings.py
@@ -82,13 +82,15 @@ WSGI_APPLICATION = 'spotify_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
-        conn_max_age=600,
-        ssl_require=True,
-    )
-}
+DATABASES = {}
+
+if os.getenv("DATABASE_URL"):
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+else:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
